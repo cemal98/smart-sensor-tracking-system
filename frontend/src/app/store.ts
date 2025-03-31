10c25c5ad303d44069d533f6f45f923d1754b851
Cemal Kılıçkaya <cemalkilickaya@Cemal-MacBook-Air.local>
2025-03-31 16:59:07 +0300

    initial-frontend

---
+++ frontend/src/app/store.ts
@@ -0,0 +1,19 @@
+import { configureStore } from "@reduxjs/toolkit";
+import createSagaMiddleware from "redux-saga";
+import rootSaga from "../sagas/rootSaga";
+
+const sagaMiddleware = createSagaMiddleware();
+
+const store = configureStore({
+  reducer: {
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(sagaMiddleware),
+});
+
+sagaMiddleware.run(rootSaga);
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+export default store;
---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+
+
+const Home: React.FC = () => {
+
+  return (
+    <div>
+      <span className="font-bold">ASD</span>
+    </div>
+  );
+};
+
+export default Home;
